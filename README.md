e143cfb0f8ada94b00cbba310d580dfc497d4d0d
geos0ft <geos0ft@users.noreply.github.com>
2021-03-13 20:25:29 +0000

    Updated
    Changes made to get the widget working. See README

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # olapic
 Repository for Olapic assessment
+1. First it was important understand what data the API returned.
+2. Once the data set was returned then it was straight forward to understand how to drill down to the media array component. Our are of interest was under data -> _embedded -> media. The actual userable image could then be obtained under images -> normal for each media element.
+3. To select the most appropriate image, the normal component needed to be chosen.
+4. There was an issue with the code base in that the last line of the iteration loop was included along with closing parenthesis and this caused some confusion.
+5. The width of the carousel was not sufficient to display all four images swas increase to max.
+6. For the navigation arrows, the parameter was set to false by default so needed to be set to true. Also the arrows were located in the middle of the carousel to classes owl-prev and .owl-next of the DOM needed to be configured appropriately.
--- olapic-js-assessment-201906/js/script.js
+++ olapic-js-assessment-201906/js/script.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
   /******** Homepage Carousel - All Photos *********/
 
   // Build API Request
-  var olapicEndpoint = "";
+  var olapicEndpoint = "https://photorankapi-a.akamaihd.net/customers/215757/media/recent?rights_given=0&auth_token=0a40a13fd9d531110b4d6515ef0d6c529acdb59e81194132356a1b8903790c18&version=v2.2";
 
   $.ajax({
     dataType: "json",
@@ -15,10 +15,18 @@ $(document).ready(function() {
     success: function(data) {     
       
       // Create array of photos pulled from the Olapic API response
-      var mediaArray = ;
+      var mediaArray = data.data._embedded.media;
+
+      // Define string for images
+      var olapicImage = "";
 
       // Loop through the mediaArray
       
+      for (x=0; x < mediaArray.length; x++) {
+        var mediaItem = mediaArray[x];
+      
+        // Define a image 
+        var olapicImage = mediaItem.images.normal;
 
       // Append the images to the carousel
         $(".owl-carousel").append("<div class='item olapic-image'><img src='" + olapicImage + "'></div>");
@@ -29,7 +37,7 @@ $(document).ready(function() {
         items: 4,
         loop: true,
         touchDrag: true,
-        nav: false,
+        nav: true,
         navText: ["<i class='arrow left'></i>", "<i class='arrow right'></i>"],
         dots: false,
         video: true,
@@ -44,7 +52,7 @@ $(document).ready(function() {
             center:true
           },
           414: {
-            items: 2
+            items:2
           },
           550: {
             items:3
